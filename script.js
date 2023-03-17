97786b96e51d76e642d916bee5b98901e5d42f39
ImeneRdn <imene.ramdan@live.fr>
2023-03-17 15:27:31 +0100

    remplissagechamps

---
+++ script.js
@@ -0,0 +1,7 @@
+const contact = document.querySelector('.send');
+const message = document.querySelector('#message');
+
+contact.addEventListener('click', function () {
+    alert("Votre message est envoyé !");
+});
+message.addEventListener
