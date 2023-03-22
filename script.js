7e43b6edf904058ce7ff25864da34f5592e45dea
ImeneRdn <imene.ramdan@live.fr>
2023-03-22 11:07:29 +0100

    modifichecontact

--- script.js
+++
@@ -1,7 +0,0 @@
-const contact = document.querySelector('.send');
-const message = document.querySelector('#message');
-
-contact.addEventListener('click', function () {
-    alert("Votre message est envoyé !");
-});
-message.addEventListener
