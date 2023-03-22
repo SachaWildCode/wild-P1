bcb467d20b845a9a21e7ec3c90fbe58ab8f3ddf1
SachaWildCode <sacha.loumachi@gmail.com>
2023-03-22 14:57:38 +0100

    fix

--- scripts/data.js
+++ scripts/data.js
@@ -1,17 +1,3 @@
-//Data from here
-/*const url = "http://127.0.0.1:3000/restaurants.json";
-const json = fetch(url)
-  .then((response) => response.json())
-  .then((json) => {
-    for (let i = 0; i < json.length; i++) {
-      console.log(
-        json[i].uuid + json[i].fulladdr + json[i].name + json[i].categories
-      );
-    }
-    return json;
-});
-*/
-
 let jsondata;
 fetch("resources/restaurants.json")
   .then(function (u) {
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,43 +1,64 @@
-const categorieslist = [
-  "burger",
-  "pizza",
-  "chinois",
-  "japonais",
-  "thaï",
-  "français",
-];
+import jsondata from "./data";
+
+const categorieslist = ["burger", "italien", "asiatique", "français", "tout"];
 const regimelist = ["halal", "vegan", "sans gluten"];
 const trierlist = ["note"];
-window.onload = () => {
-  clickevent();
-};
-const clickevent = () => {
-  let regime = "none";
-  let trier = "none";
-  let categorie = "none";
+let regime = "none";
+let trier = "none";
+let type = "none";
+
+const validateEvent = () => {
   document
     .querySelector(".button-container")
     .addEventListener("click", function (e) {
       let clicked = e.target.innerHTML.toLowerCase();
       if (categorieslist.includes(clicked)) {
-        console.log(clicked);
-        categorie = clicked;
+        type = clicked;
+        console.log(type);
       }
       if (regimelist.includes(clicked)) {
-        console.log(clicked);
         regime = clicked;
       }
       if (trierlist.includes(clicked)) {
-        console.log(clicked);
         trier = clicked;
       }
-      if (clicked == "valider" && categorie !== "none") {
-        console.log(
-          `Categories : ${categorie}, Régime : ${regime}, Trier par : ${trier}`
-        );
+      if (clicked == "valider" && type !== "none") {
+        loadmenu();
       }
-      if (clicked == "valider" && categorie == "none") {
+      if (clicked == "valider" && type == "none") {
         alert("Merci de bien vouloir choisir une catégorie.");
       }
     });
+};
+
+function loadmenu() {
+  for (let i = 0; i < jsondata.length; i++) {
+    if (jsondata[i].categories.toLowerCase().includes(type)) {
+      const image = document.createElement("img");
+      image.setAttribute("draggable", false);
+      image.alt = "img";
+      if (jsondata[i].thumbnail.includes("streetviewpixels")) {
+        image.src = "resources/notavailable.png";
+      } else {
+        image.src = jsondata[i].thumbnail;
+      }
+      document.querySelector(".carousel").appendChild(image);
+    } else {
+      if (type == "tout") {
+        const image = document.createElement("img");
+        image.setAttribute("draggable", false);
+        image.alt = "img";
+        if (jsondata[i].thumbnail.includes("streetviewpixels")) {
+          image.src = "resources/notavailable.png";
+        } else {
+          image.src = jsondata[i].thumbnail;
+        }
+        document.querySelector(".carousel").appendChild(image);
+      }
+    }
+  }
+}
+
+window.onload = () => {
+  validateEvent();
 };
